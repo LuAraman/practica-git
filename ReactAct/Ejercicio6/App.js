9eb9cb37aff62eb79d09c92588ceba588b99c4f8
LuAraman <armandodlcruzmontes22@gmail.com>
2025-10-31 08:39:00 -0600

    Add files via upload

---
+++ ReactAct/Ejercicio6/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, FlatList } from 'react-native';
+
+export default function App() {
+  const frutas = ["Manzana", "Plátano", "Naranja", "Uvas"];
+
+  return (
+    <View style={{ flex:1, padding:20 }}>
+      <FlatList 
+        data={frutas}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => <Text style={{ fontSize:20, margin:5 }}>{item}</Text>}
+      />
+    </View>
+  );
+}
